7194417d534e2b34169d90af2193d88431f05bf5
jumpingRock <kennethyeongky@gmail.com>
2024-03-30 17:27:20 +0800

    added migration files

---
+++ db/migration/000002_add_users.down.sql
@@ -0,0 +1,5 @@
+AlTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "owner_currency_key";
+
+AlTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "accounts_owner_fkey";
+
+DROP TABLE IF EXISTS users
